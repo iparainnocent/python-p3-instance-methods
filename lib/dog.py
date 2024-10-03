cd327446c5121144e5de717c81eddf0cfd3ea5c1
innocent <iparainnocent@gmail.com>
2024-10-03 14:48:08 +0300

    commit changes

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,12 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+    def bark(self):
+        print("Woof!")
+fido = Dog()
+fido.sit()
+
+    
+
